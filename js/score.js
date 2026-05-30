725a17dbdb98d4af84aa0faffa786b41bb3c5f74
Sas4trexs <filipesbartolo@gmail.com>
2026-05-30 11:21:33 +0100

    Update score.js

--- js/score.js
+++ js/score.js
@@ -1,7 +1,7 @@
 // ─── CONFIGURAÇÕES AJUSTÁVEIS ────────────────────────────────
-const LIST_SIZE = 100;    // 👈 Aumente aqui ao adicionar fases
-const BASE_POINTS = 250; // Pontos máximos para o rank 1
-const DECAY = 0.964;     // Queda por rank (menor = mais agressivo)
+const LIST_SIZE = 90;    // 👈 Aumente aqui ao adicionar fases
+const BASE_POINTS = 270; // Pontos máximos para o rank 1
+const DECAY = 0.970;     // Queda por rank (menor = mais agressivo)
 // ─────────────────────────────────────────────────────────────
 
 const scale = 3;
